e5ff94ece0e1e98eee488671b63cbbfd7f7542c4
LzsCxb <lzscxb@gmail.com>
2022-02-22 16:46:45 +0800

    程序退出注销服务

--- user_srv/server.py
+++ user_srv/server.py
@@ -7,6 +7,7 @@ from concurrent import futures
 import socket
 import grpc
 from loguru import logger
+from functools import partial
 
 # 因为在终端上运行会找不到根目录所以需要指明项目根目录
 BASE_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
@@ -20,11 +21,11 @@ from common.grpc_health.v1 import health
 from common.register.consul import ConsulRegister
 
 
-def on_exit(signal, frame):
+def on_exit(signal, frame, service_id):
     logger.info("grpc server exit")
     logger.info("服务注消中...")
     c = ConsulRegister(host=server_config.CONSUL_HOST, port=server_config.CONSUL_PORT)
-    if not c.deregister(service_id=server_config.SERVER_NAME):
+    if not c.deregister(service_id=service_id):
         logger.error("服务注销失败")
     logger.info(f"{server_config.SERVER_NAME}服务已注销")
     sys.exit(0)
@@ -50,11 +51,10 @@ def serve():
     parser.add_argument('--port',
                         nargs="?",
                         type=int,
-                        default=get_free_tcp_port(),# 动态获取
+                        default=get_free_tcp_port(),  # 动态获取
                         help="the listening port"
                         )
     args = parser.parse_args()
-
 
     logger.add("logs/user_srv_{time}.log")
     # 多线程启动
@@ -66,23 +66,25 @@ def serve():
     # 端口
     # server.add_insecure_port("[::]:50051")
     server.add_insecure_port(f'{args.ip}:{args.port}')
+    import uuid
+    service_id = str(uuid.uuid1())
     # 监听退出信号
     """
         windows下支持的信号是有限的：
             SIGINT ctrl+C终端
             SIGTERM kill发出的软件终止
+    因为 on_exit 前两个参数是自动注入的，所以需要使用partial重新包装一下
     """
-    signal.signal(signal.SIGINT, on_exit)
-    signal.signal(signal.SIGTERM, on_exit)
+    signal.signal(signal.SIGINT, partial(on_exit,service_id=service_id))
+    signal.signal(signal.SIGTERM, partial(on_exit,service_id=service_id))
 
     logger.info(f"启动服务: {args.ip}:{args.port}")
     server.start()
 
     logger.info("服务注册中...")
     c = ConsulRegister(host=server_config.CONSUL_HOST, port=server_config.CONSUL_PORT)
-    success = c.register(name=server_config.SERVER_NAME, service_id=server_config.SERVER_NAME, address=server_config.HOST,
-               port=server_config.PORT, tags=server_config.SERVER_TAGS, check=None)
-    print(success)
+    success = c.register(name=server_config.SERVER_NAME, service_id=service_id, address=args.ip,
+                         port=args.port, tags=server_config.SERVER_TAGS, check=None)
     if not success:
         logger.error("服务注册失败")
         sys.exit(0)
